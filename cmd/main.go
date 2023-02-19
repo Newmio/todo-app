14b6254548cdcd492a034d4723626cec72716480
Newmio <newm.dev@gmail.com>
2023-02-19 20:59:06 +0200

    Структура проекта

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	todo_app "github.com/Newmio/todo-app"
+	"github.com/Newmio/todo-app/pkg/handler"
+	"log"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+	srv := new(todo_app.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
