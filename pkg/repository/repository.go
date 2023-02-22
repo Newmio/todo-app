489f0e97a42914b9d1f2006fad73a23aaf08482a
Newmio <newm.dev@gmail.com>
2023-02-23 00:18:52 +0200

    схема бд, логика для нового юзера

---
+++ pkg/repository/repository.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	todo_app "github.com/Newmio/todo-app"
+	"github.com/jmoiron/sqlx"
+)
+
+type Authorization interface {
+	CreateUser(user todo_app.User) (int, error)
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Repository struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{
+		Authorization: NewAuthPostgres(db),
+	}
+}
