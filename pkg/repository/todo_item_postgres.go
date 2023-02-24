2774b66fe84ddf6f09fbc6f8885400f49f64b1e4
Newmio <newm.dev@gmail.com>
2023-02-24 21:36:20 +0200

    items

---
+++ pkg/repository/todo_item_postgres.go
@@ -0,0 +1,100 @@
+package repository
+
+import (
+	"fmt"
+	todo_app "github.com/Newmio/todo-app"
+	"github.com/jmoiron/sqlx"
+	"strings"
+)
+
+type TodoItemPostgres struct {
+	db *sqlx.DB
+}
+
+func NewTodoItemPostgres(db *sqlx.DB) *TodoItemPostgres {
+	return &TodoItemPostgres{db: db}
+}
+
+func (r *TodoItemPostgres) Create(listId int, item todo_app.TodoItem) (int, error) {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return 0, err
+	}
+
+	var itemId int
+	createItemQuery := fmt.Sprintf("insert into %s (title, description) values ($1, $2) RETURNING id", todoItemsTable)
+
+	row := tx.QueryRow(createItemQuery, item.Title, item.Description)
+	err = row.Scan(&itemId)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	createListItemsQuery := fmt.Sprintf("insert into %s (list_id, item_id) values ($1, $2)", listsItemsTable)
+	_, err = tx.Exec(createListItemsQuery, listId, itemId)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	return itemId, tx.Commit()
+}
+
+func (r *TodoItemPostgres) GetAll(userId, listId int) ([]todo_app.TodoItem, error) {
+	var items []todo_app.TodoItem
+	query := fmt.Sprintf("select ti.id, ti.title, ti.description, ti.done from %s ti inner join %s li on li.item_id = ti.id inner join %s ul on ul.list_id = li.list_id where li.list_id = $1 and ul.user_id = $2", todoItemsTable, listsItemsTable, usersListsTable)
+	if err := r.db.Select(&items, query, listId, userId); err != nil {
+		return nil, err
+	}
+
+	return items, nil
+}
+
+func (r *TodoItemPostgres) GetById(userId, itemId int) (todo_app.TodoItem, error) {
+	var item todo_app.TodoItem
+	query := fmt.Sprintf("select ti.id, ti.title, ti.description, ti.done from %s ti inner join %s li on li.item_id = ti.id inner join %s ul on ul.list_id = li.list_id where ti.id = $1 and ul.user_id = $2", todoItemsTable, listsItemsTable, usersListsTable)
+	if err := r.db.Get(&item, query, itemId, userId); err != nil {
+		return item, err
+	}
+
+	return item, nil
+}
+
+func (r *TodoItemPostgres) Delete(userId, itemId int) error {
+	query := fmt.Sprintf("delete from %s ti USING %s li, %s ul where ti.id = li.item_id and li.list_id = ul.list_id and ul.user_id = $1 and ti.id = $2", todoItemsTable, listsItemsTable, usersListsTable)
+	_, err := r.db.Exec(query, userId, itemId)
+	return err
+}
+
+func (r *TodoItemPostgres) Update(userId, itemId int, input todo_app.UpdateItemInput) error {
+	setValues := make([]string, 0)
+	args := make([]interface{}, 0)
+	argId := 1
+
+	if input.Title != nil {
+		setValues = append(setValues, fmt.Sprintf("title=$%d", argId))
+		args = append(args, *input.Title)
+		argId++
+	}
+
+	if input.Description != nil {
+		setValues = append(setValues, fmt.Sprintf("description=$%d", argId))
+		args = append(args, *input.Description)
+		argId++
+	}
+
+	if input.Done != nil {
+		setValues = append(setValues, fmt.Sprintf("done=$%d", argId))
+		args = append(args, *input.Done)
+		argId++
+	}
+
+	setQuery := strings.Join(setValues, ", ")
+
+	query := fmt.Sprintf("update %s ti set %s from %s li, %s ul where ti.id = li.item_id and li.list_id = ul.list_id and ul.user_id = $%d and ti.id = $%d", todoItemsTable, setQuery, listsItemsTable, usersListsTable, argId, argId+1)
+	args = append(args, userId, itemId)
+
+	_, err := r.db.Exec(query, args...)
+	return err
+}
