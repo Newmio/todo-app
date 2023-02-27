20b1c118d98b7a0493ae20594e9fa0ca027a464b
Newmio <newm.dev@gmail.com>
2023-02-27 16:36:38 +0200

    swagger FIX

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	_ "github.com/Newmio/todo-app/docs"
 	"github.com/Newmio/todo-app/pkg/service"
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
 )
 
 type Handler struct {
@@ -15,6 +18,8 @@ func NewHandler(services *service.Service) *Handler {
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	auth := router.Group("/auth")
 	{
